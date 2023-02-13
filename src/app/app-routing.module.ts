ba8eec87756e1d8afb14cee1a4a4eeb08e715efa
Erick Hernandez <yerickk8@gmail.com>
2023-02-13 11:05:42 -0600

    Added watch-list and add-movie components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddMovieComponent } from './components/add-movie/add-movie/add-movie.component';
+import { WatchListComponent } from './components/watch-list/watch-list/watch-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WatchListComponent },
+  { path: 'addMovie', component: AddMovieComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
